9f901a3a29533d1ad12de66da9ede089461c828a
Chris Murphy <chris@strandz.org>
2016-05-08 03:16:21 +1000

    some progress

--- src/slides/core.cljs
+++ src/slides/core.cljs
@@ -7,16 +7,6 @@
             [cljs.test :refer-macros [deftest is testing run-tests]]
             [intro.components :as comps :refer [->ShowList]]))
 
-(defclj code-slide 40
-  (def a (+ 1 2))
-  (def b (+ a 3))
-  (defn c [d] (+ d b a))
-  (defn d [e]
-    (if (= e :do-it)
-      (c 10)
-      (c 15)))
-  (d :do-it))
-
 (defshow om-next-client-side
          default-style
          (->Title
@@ -25,33 +15,30 @@
            "/pictures/dog_food.jpg"
            "Om Next")
          (->ShowList ["component" "query" "query keyword" "default db format" "ident" "ident keyword"])
+         (pseudo-clj 60
+                     (defui AnyAction
+                            Object
+                            (render [this]
+                                    (let [{:keys [text action]} (om/props this)]
+                                      (dom/button #js{:onClick action} text))))
+                     (def any-action (om/factory AnyAction))
+                     (any-action {:text "Show State" :action #(pprint @my-reconciler)}))
          (pseudo-clj 80
-                     (defshow show-name
-                              slide-style
-                              (->Title "title" "subtitle")
-                              (->Text "text")
-                              (->Picture "url")
-                              (->CaptionedPic "url" "text")))
-         (->Text "Most of our technical slide shows are just plain strings or files!")
-         (->Text "What if we could write techincal presentations in actual testable code!")
-         code-slide
+                     [:id :line-name :selected?])
+         (pseudo-clj 40
+                     (defui Checkbox
+                            static om/IQuery
+                            (query [this]
+                                   [:id :line-name :selected?])))
          (pseudo-clj 80
-                     (defclj code-slide 40
-                             (def a (+ 1 2))
-                             (def b (+ a 3))
-                             (defn c [d] (+ d b a))
-                             (defn d [e]
-                               (if (= e :do-it)
-                                 (c 10)
-                                 (c 15)))
-                             (d :do-it)))
-         (pseudo-clj 80
-                     (deftest code-slide-test
-                       (is (= (get (:env show/code-slide) :a) 3))
-                       (is (= (get (:env show/code-slide) :b) 6))
-                       (is (= ((get (:env show/code-slide) :c) 1) 10))
-                       (is (= (get (:env show/code-slide) 4) 19))))
-         (->Text "Tests run live in a figwheel environment while you develop.")
+                     (defui Root
+                            static om/IQuery
+                            (query [this]
+                                   [{:app/app-lines (om/get-query Checkbox)}
+                                    {:graph/fake-graph (om/get-query FakeGraph)}
+                                    {:graph/graph-lines (om/get-query Line)}
+                                    ])))
+         (->Text "Follow :graph/graph-lines query keyword to init-state and @my-reconciler")
          (->Text "The Slide Abstraction: How to Extend Jobim")
          (pseudo-clj 80
                      (defprotocol Slide
